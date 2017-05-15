4b1ae02177e5a28a40b8f33660083825d5a7195a
vit <vitalii.khodak@gmail.com>
2017-05-15 10:42:12 +0300

    very first skeleton

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.listen(8080);
+
+/*
+
+ FOLDER STRUCTURE:
+
+ root
+ app
+ server
+ server.js
+ package.json
+ index.html
+ package.json
+
+ */
+  
